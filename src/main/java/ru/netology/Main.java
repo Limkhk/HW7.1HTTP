8884c52da5dee661b2ad6f09d73066357e093b9f
Averkin S.S <ssaverkin@ya.ru>
2023-11-02 21:36:05 +0300

    add class RequestHandler

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -7,7 +7,7 @@ public class Main {
         final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
         // Создаем экземпляр Server с 64 потоками
-        final var server = new Server(validPaths, 64);
+        final var server = new Server(validPaths, 64,9999);
 
         // Запуск сервера
         server.start();
